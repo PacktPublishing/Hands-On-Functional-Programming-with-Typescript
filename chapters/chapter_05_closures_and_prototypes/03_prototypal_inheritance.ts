c95b6844b12a7c1ac2404aaa2b6f5bbb3c556f80
Remo H. Jansen <remo.jansen@wolksoftware.com>
2019-01-28 09:30:10 +0000

    Update 03_prototypal_inheritance.ts

--- chapters/chapter_05_closures_and_prototypes/03_prototypal_inheritance.ts
+++ chapters/chapter_05_closures_and_prototypes/03_prototypal_inheritance.ts
@@ -1,26 +1,46 @@
 namespace prototypal_inheritance_demo_1 {
 
-    const Person = prototypes_demo_1.Person;
+    class Person {
+
+        public name: string;
+        public surname: string;
+        public age: number = 0;
+
+        public constructor(name: string, surname: string) {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public greet() {
+            let msg = `Hi! my name is ${this.name} ${this.surname}`;
+            msg += `I'm ${this.age}`;
+        }
+
+    }
 
     class SuperHero extends Person {
 
-        public superpower: string;
-    
-        public constructor(
-            name: string,
-            surname: string,
-            superpower: string
-        ) {
-            super(name, surname);
-            this.superpower = superpower;
-        }
-    
-        public userSuperPower() {
-            return `I'm using my ${this.superpower}`;
-        }
-    
+            public superpower: string;
+
+            public constructor(
+                name: string,
+                surname: string,
+                superpower: string
+            ) {
+                super(name, surname);
+                this.superpower = superpower;
+            }
+
+            public userSuperPower() {
+                return `I'm using my ${this.superpower}`;
+            }
+
     }
 
-    // Please refer to compilation output
+    const hero = new SuperHero("Peter", "Parker", "Spider-sense");
+    hero.greet();
+    hero.userSuperPower();
+
+    // Please refer to the compilation output to learn more about prototypes
 
 }
